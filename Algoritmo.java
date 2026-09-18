8213746d80fdef79ed9eb94fbf1b91a2a12f2800
javirojas988 <jrojagu988@g.educaand.es>
2026-09-18 14:40:51 +0200

    adad

--- Algoritmo.java
+++ Algoritmo.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Algoritmo {
 
     public static void main(String[] args) {
+        String nombre = "cristian";
+        int numero ;
+        
+        
+        System.out.println("\033[41moye mi papu, te he de decir que me introduzcas un numero y te dire quien es la perrita mas obediente ese numero de veces, >3\033[0m");
+        Scanner sc = new  Scanner(System.in);
+        numero = sc.nextInt() ; 
 
-        String nombre = "papu"; 
+        System.out.print("\033[32mPErrita a continuacion\n\033[0m");
 
-        System.out.println(nombre);
+        for ( int i= 0 ; i < numero ; i++ ){
+            System.out.printf(nombre+" ");
+        }
         
     }
 }
---
+++ unidad1/Ejercicio1.java
@@ -0,0 +1,48 @@
+package unidad1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.printf("\n|   lunes  |  martes  | miercoles|  jueves  |  viernes |\n");
+        System.out.printf("========================================================\n");
+
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , "nada" , "nada","bdds","nada");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "prog" , "nada" , "prog","bdds","nada");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "prog" , "ipe2" , "prog","nada","nada");
+        System.out.printf("|------------------------------------------------------|\n");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , "bdds" , "prog","nada","prog");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , "bdds" , "bdds","ipe2","prog");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , "nada" , "bdds","ipe2","prog");
+        System.out.printf("========================================================\n");
+        
+        //
+        String prog = "\033[31mProg\033[0m";
+        String bdd = "\033[32mbdds\033[0m";
+        String ipe2 = "\033[33mipe2\033[0m";
+        
+        System.out.printf("\n|   lunes  |  martes  | miercoles|  jueves  |  viernes |\n");
+        System.out.printf("========================================================\n");
+
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , "nada" , "nada",bdd,"nada");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", prog , "nada" , prog,bdd,"nada");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", prog , ipe2 , prog,"nada","nada");
+        System.out.printf("|------------------------------------------------------|\n");
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , bdd , prog,"nada",prog);
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , bdd , bdd,ipe2,prog);
+        System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", "nada" , "nada" , bdd,ipe2,prog);
+        System.out.printf("========================================================\n");
+
+        //
+        int i = 1 ;
+        int a = 0 ;
+        System.out.printf("\n|   lunes  |  martes  | miercoles|  jueves  |  viernes |\n");
+        System.out.printf("========================================================\n");
+        String asignaturas[] = {"nada" , "nada" , "nada", bdd ,"nada", prog , "nada" , prog,bdd,"nada",prog , ipe2 , prog,"nada","nada",prog , ipe2 , prog,"nada","nada","nada" , bdd , prog,"nada",prog,"nada" , bdd , bdd,ipe2,prog,"nada" , "nada" , bdd,ipe2,prog};
+        while ( i <= 7) {
+            //meter fila recreo 
+            System.out.printf("|   %s   |   %s   |   %s   |   %s   |   %s   |\n", asignaturas[a], a++, asignaturas[a], a++, asignaturas[a],a++, asignaturas[a],a++, asignaturas[a] );
+            
+        }
+    }
+
+    
+}
